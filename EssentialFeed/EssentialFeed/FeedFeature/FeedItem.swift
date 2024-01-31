5a1c4a6d0a0e5572e2dd7b1f9868a8a2ca19fb83
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-01-31 20:31:02 +0400

    ‘RemoteFeedLoader’ does not request data upon creation

---
+++ EssentialFeed/EssentialFeed/FeedFeature/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Afsal on 31/01/2024.
+//
+
+import Foundation
+
+struct FeedItem {
+  let id: UUID
+  let description: String?
+  let location: String?
+  let imageURL: URL
+}
