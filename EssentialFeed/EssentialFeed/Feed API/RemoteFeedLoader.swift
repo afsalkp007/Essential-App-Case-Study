f840d27cd87a0c771e3de7ecea0ccae1dd1661a9
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-02-01 16:21:51 +0400

    move decodable logic to new private item

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -40,7 +40,7 @@ public final class RemoteFeedLoader {
       switch result {
       case let .success(data, response):
         if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
-          completion(.success(root.items))
+          completion(.success(root.items.map { $0.item }))
         } else {
           completion(.failure(.invlalidData))
         }
@@ -51,17 +51,26 @@ public final class RemoteFeedLoader {
   }
 }
 
+private class FeedItemsMapper {
+  static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+    guard response.statusCode == 200 else {
+      throw RemoteFeedLoader.Error.invlalidData
+    }
+    return try JSONDecoder().decode(Root.self, from: data).items.map { $0.item }
+  }
+}
+
 private struct Root: Decodable {
-  let items: [FeedItem]
+  let items: [Item]
 }
 
 private struct Item: Decodable {
   let id: UUID
   let description: String?
   let location: String?
-  let imageURL: URL
+  let image: URL
   
   var item: FeedItem {
-    FeedItem(id: id, description: description, location: location, imageURL: imageURL)
+    FeedItem(id: id, description: description, location: location, imageURL: image)
   }
 }
