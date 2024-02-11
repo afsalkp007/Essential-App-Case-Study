606aa3da9f18e76f5a31753a0e084c50e6db5682
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-02-11 13:58:26 +0400

    extract duplicate retrieve method with a helper method

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -77,22 +77,10 @@ class CodableFeedStoreTests: XCTestCase {
   
   func test_retrieve_deliversEmptyOnEmptyCache() {
     let sut = makeSUT()
-    let exp = expectation(description: "Wait for cache retrieval")
-
-    sut.retrieve { result in
-      switch result {
-      case .empty:
-        break
-
-      default:
-        XCTFail("Expected empty result, got \(result) instead")
-      }
-
-      exp.fulfill()
-    }
-
-    wait(for: [exp], timeout: 1.0)
+    
+    expect(sut, toRetrieve: .empty)
   }
+  
   func test_retrieve_hasNoSideEffectsOnEmptyCache() {
     let sut = makeSUT()
     let exp = expectation(description: "Wait for cache retrieval")
@@ -118,23 +106,35 @@ class CodableFeedStoreTests: XCTestCase {
     let sut = makeSUT()
     let feed = uniqueImageFeed().local
     let timestamp = Date()
+    
     let exp = expectation(description: "Wait for cache retrieval")
-
     sut.insert(feed, timestamp: timestamp) { insertionError in
       XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
+      exp.fulfill()
+    }
+    wait(for: [exp], timeout: 1.0)
+    
+    expect(sut, toRetrieve: .found(feed: feed, timestamp: timestamp))
 
-      sut.retrieve { retrieveResult in
-        switch retrieveResult {
-        case let .found(retrievedFeed, retrievedTimestamp):
-          XCTAssertEqual(retrievedFeed, feed)
-          XCTAssertEqual(retrievedTimestamp, timestamp)
+  }
+  
+  private func expect(_ sut: CodableFeedStore, toRetrieve expectedResult: RetrievedCachedFeedResult, file: StaticString = #filePath, line: UInt = #line) {
+    let exp = expectation(description: "Wait for cache retrieval")
+    
+    sut.retrieve { retrieveResult in
+      switch (expectedResult, retrieveResult) {
+      case (.empty, .empty):
+        break
+        
+      case let (.found(expectedFeed, expectedTimestamp), .found(retrievedFeed, retrievedTimestamp)):
+        XCTAssertEqual(retrievedFeed, expectedFeed, file: file, line: line)
+        XCTAssertEqual(retrievedTimestamp, expectedTimestamp, file: file, line: line)
 
-        default:
-          XCTFail("Expected found result with feed \(feed) and timestamp \(timestamp), got \(retrieveResult) instead")
-        }
-
-        exp.fulfill()
+      default:
+        XCTFail("Expected found result with feed and timestamp, got instead")
       }
+
+      exp.fulfill()
     }
 
     wait(for: [exp], timeout: 1.0)
