e5f800c2d44cda00ce15132325e5935e8a32cdf4
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-02-12 16:24:06 +0400

    Clean up & undo all cache side-effects on setUp and tearDown to a…

--- EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -9,6 +9,19 @@ import XCTest
 import EssentialFeed
 
 class EssentialFeedCacheIntegrationTests: XCTestCase {
+  
+  override func setUp() {
+    super.setUp()
+
+    setupEmptyStoreState()
+  }
+
+  override func tearDown() {
+    super.tearDown()
+
+    undoStoreSideEffects()
+  }
+
 
   func test_load_deliversNoItemsOnEmptyCache() {
     let sut = makeSUT()
@@ -45,6 +58,18 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
 
   private func cachesDirectory() -> URL {
     return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+  }
+
+  private func setupEmptyStoreState() {
+    deleteStoreArtifacts()
+  }
+
+  private func undoStoreSideEffects() {
+    deleteStoreArtifacts()
+  }
+
+  private func deleteStoreArtifacts() {
+    try? FileManager.default.removeItem(at: testSpecificStoreURL())
   }
 
 }
