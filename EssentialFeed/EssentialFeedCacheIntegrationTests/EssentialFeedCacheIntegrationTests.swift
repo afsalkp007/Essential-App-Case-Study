8e0c4e084fb371f2e5d36f783e78c31cd53ed4ad
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-02-12 15:47:57 +0400

    add cache integration tests

---
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Afsal on 12/02/2024.
+//
+
+import XCTest
+
+class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+
+}
