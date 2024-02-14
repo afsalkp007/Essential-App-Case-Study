374a58ac5bb101caabe9c30e6e2b4329621f3311
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-02-14 15:37:29 +0400

    added view controller rendering

---
+++ Prototype/Prototype/FeedViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FeedViewController.swift
+//  Prototype
+//
+//  Created by Afsal on 14/02/2024.
+//
+
+import UIKit
+
+final class FeedViewController: UITableViewController {
+
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 10
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return tableView.dequeueReusableCell(withIdentifier: "FeedImageCell")!
+  }
+}
