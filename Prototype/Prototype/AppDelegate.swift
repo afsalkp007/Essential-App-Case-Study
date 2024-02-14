b56bd63f33063435e99045f7b099cb8828b54ab3
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-02-14 13:17:04 +0400

    create single view app for prototype

---
+++ Prototype/Prototype/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Prototype
+//
+//  Created by Afsal on 14/02/2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+}
+
