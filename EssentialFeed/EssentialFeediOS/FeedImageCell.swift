acace2ba9c07f1845295c6db421dd8eb33f1cacb
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-02-16 08:02:54 +0400

    Render loaded feed on successful load completion

---
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Afsal on 16/02/2024.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+  public let locationContainer = UIView()
+  public let locationLabel = UILabel()
+  public let descriptionLabel = UILabel()
+}
+ 
