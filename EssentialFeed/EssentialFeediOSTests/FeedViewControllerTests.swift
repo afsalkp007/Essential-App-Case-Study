6de0cb271d533f35acca4195ef250f74f1df137d
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-02-18 12:28:13 +0400

    Reuse table view cells from storyboard configuration

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -239,6 +239,16 @@ final class FeedViewControllerTests: XCTestCase {
     XCTAssertEqual(loader.cancelledImageURLs, [image0.url, image1.url], "Expected second cancelled image URL request once second image is not near visible anymore")
   }
 
+  func test_feedImageView_doesNotRenderLoadedImageWhenNotVisibleAnymore() {
+    let (sut, loader) = makeSUT()
+    sut.loadViewIfNeeded()
+    loader.completeFeedLoading(with: [makeImage()])
+
+    let view = sut.simulateFeedImageViewNotVisible(at: 0)
+    loader.completeImageLoading(with: anyImageData())
+
+    XCTAssertNil(view?.renderedImage, "Expected no rendered image when an image load finishes after the view is not visible anymore")
+  }
 
   // MARK: - Helpers
   
@@ -249,6 +259,11 @@ final class FeedViewControllerTests: XCTestCase {
     trackForMemoryLeaks(sut, file: file, line: line)
     return (sut, loader)
   }
+  
+  private func anyImageData() -> Data {
+    return UIImage.make(withColor: .red).pngData()!
+  }
+
   private func assertThat(_ sut: FeedViewController, isRendering feed: [FeedImage], file: StaticString = #file, line: UInt = #line) {
     guard sut.numberOfRenderedFeedImageViews() == feed.count else {
       return XCTFail("Expected \(feed.count) images, got \(sut.numberOfRenderedFeedImageViews()) instead.", file: file, line: line)
@@ -343,12 +358,15 @@ private extension FeedViewController {
     return feedImageView(at: index) as? FeedImageCell
   }
 
-  func simulateFeedImageViewNotVisible(at row: Int) {
+  @discardableResult
+  func simulateFeedImageViewNotVisible(at row: Int) -> FeedImageCell? {
     let view = simulateFeedImageViewVisible(at: row)
 
     let delegate = tableView.delegate
     let index = IndexPath(row: row, section: feedImagesSection)
     delegate?.tableView?(tableView, didEndDisplaying: view!, forRowAt: index)
+    
+    return view
   }
   
   func simulateFeedImageViewNearVisible(at row: Int) {
