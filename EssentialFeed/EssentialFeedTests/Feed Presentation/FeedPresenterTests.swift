851421bb3cd1596d822ebcb2279eeefa7b8cd25c
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-02-19 14:19:18 +0400

    Move FeedLocalizationTests to cross-platform EssentialFeed module

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -59,7 +59,7 @@ class FeedPresenterTests: XCTestCase {
   
   private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedPresenter, view: ViewSpy) {
     let view = ViewSpy()
-    let sut = FeedPresenter(feedView: view, errorView: view, loadingView: view)
+    let sut = FeedPresenter(feedView: view, loadingView: view, errorView: view)
     trackForMemoryLeaks(view, file: file, line: line)
     trackForMemoryLeaks(sut, file: file, line: line)
     return (sut, view)
