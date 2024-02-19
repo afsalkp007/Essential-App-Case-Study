1035ef4de7b9a90cc71c0f0488b5585497720ec3
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-02-19 12:39:40 +0400

    Configure ErrorView layout in storyboard

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/View/ErrorView.swift
@@ -0,0 +1,23 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Afsal on 19/02/2024.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+  @IBOutlet private var label: UILabel!
+
+  public var message: String? {
+    get { return label.text }
+    set { label.text = newValue }
+  }
+
+  public override func awakeFromNib() {
+    super.awakeFromNib()
+
+    label.text = nil
+  }
+}
