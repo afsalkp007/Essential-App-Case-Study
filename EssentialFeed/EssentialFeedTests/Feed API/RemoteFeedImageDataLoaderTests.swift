ae3f26dff53f7d593837d52e6b36f8c155d5b7d5
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-02-20 16:39:35 +0400

    fixes

--- EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -19,25 +19,29 @@ final class RemoteFeedImageDataLoader {
     case invalidData
   }
   
+  private struct HTTPTaskWrapper: FeedImageDataLoaderTask {
+    let wrapped: HTTPClientTask
+    
+    func cancel() {
+      wrapped.cancel()
+    }
+  }
+  
   @discardableResult
-  func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> HTTPClientTask {
-    client.get(from: url) { [weak self] result in
+  func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+    return HTTPTaskWrapper(wrapped: client.get(from: url) { [weak self] result in
       guard self != nil else { return }
       
       switch result {
       case let .success((data, response)):
-        completion(Self.map(data, response))
+        if response.statusCode == 200, !data.isEmpty {
+          completion(.success(data))
+        } else {
+          completion(.failure(Error.invalidData))
+        }
       case let .failure(error): completion(.failure(error))
       }
-    }
-  }
-  
-  static func map(_ data: Data, _ response: HTTPURLResponse) -> FeedImageDataLoader.Result {
-    if response.statusCode == 200, !data.isEmpty {
-      return .success(data)
-    } else {
-      return .failure(Error.invalidData)
-    }
+    })
   }
 }
 
