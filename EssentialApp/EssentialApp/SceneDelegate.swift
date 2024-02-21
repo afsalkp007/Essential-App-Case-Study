d57645c5ca3cd02afeace4eaf34fdd3ff3183a83
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-02-21 13:47:00 +0400

    Wait for URLProtocol to complete the URLRequest after a task has been…

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -6,13 +6,16 @@
 //
 
 import UIKit
+import EssentialFeed
+import EssentialFeediOS
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-  
   var window: UIWindow?
   
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
     guard let _ = (scene as? UIWindowScene) else { return }
+    
+    
   }
 }
 
