3edbe27125ba0047d2d0da4e2434394fa531412a
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-02-22 11:59:45 +0400

    Extract system under test (SUT) creation into a factory method

--- EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
@@ -20,61 +20,29 @@ final class FeedLoaderCacheDecorator: FeedLoader {
   }
 }
 
-class FeedLoaderCacheDecoratorTests: XCTestCase {
-
+class FeedLoaderCacheDecoratorTests: XCTestCase, FeedLoaderTestCase {
+  
   func test_load_deliversFeedOnLoaderSuccess() {
     let feed = uniqueFeed()
-    let loader = LoaderStub(result: .success(feed))
-    let sut = FeedLoaderCacheDecorator(decoratee: loader)
+    let sut = makeSUT(loaderResult: .success(feed))
 
     expect(sut, toCompleteWith: .success(feed))
   }
-
+  
   func test_load_deliversErrorOnLoaderFailure() {
-    let loader = LoaderStub(result: .failure(anyNSError()))
-    let sut = FeedLoaderCacheDecorator(decoratee: loader)
+    let sut = makeSUT(loaderResult: .failure(anyNSError()))
 
     expect(sut, toCompleteWith: .failure(anyNSError()))
   }
-
+  
   // MARK: - Helpers
 
-  private func expect(_ sut: FeedLoader, toCompleteWith expectedResult: FeedLoader.Result, file: StaticString = #file, line: UInt = #line) {
-    let exp = expectation(description: "Wait for load completion")
-
-    sut.load { receivedResult in
-      switch (receivedResult, expectedResult) {
-      case let (.success(receivedFeed), .success(expectedFeed)):
-        XCTAssertEqual(receivedFeed, expectedFeed, file: file, line: line)
-
-      case (.failure, .failure):
-        break
-
-      default:
-        XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
-      }
-
-      exp.fulfill()
-    }
-
-    wait(for: [exp], timeout: 1.0)
+  private func makeSUT(loaderResult: FeedLoader.Result, file: StaticString = #file, line: UInt = #line) -> FeedLoader {
+    let loader = FeedLoaderStub(result: loaderResult)
+    let sut = FeedLoaderCacheDecorator(decoratee: loader)
+    trackForMemoryLeaks(loader, file: file, line: line)
+    trackForMemoryLeaks(sut, file: file, line: line)
+    return sut
   }
-
-  private func uniqueFeed() -> [FeedImage] {
-    return [FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())]
-  }
-
-  private class LoaderStub: FeedLoader {
-    private let result: FeedLoader.Result
-    
-    init(result: FeedLoader.Result) {
-      self.result = result
-    }
-
-    func load(completion: @escaping (FeedLoader.Result) -> Void) {
-      completion(result)
-    }
-  }
-
 
 }
--- EssentialApp/EssentialAppTests/Helpers/SharedTestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/SharedTestHelpers.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import EssentialFeed
 
 func anyURL() -> URL {
   return URL(string: "http://a-url.com")!
@@ -18,3 +19,8 @@ func anyNSError() -> NSError {
 func anyData() -> Data {
   return Data("any data".utf8)
 }
+
+func uniqueFeed() -> [FeedImage] {
+  return [FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())]
+}
+
