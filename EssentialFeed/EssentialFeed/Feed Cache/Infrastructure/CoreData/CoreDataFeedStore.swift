9339d2e54fae9f8a3a5d4aef59f4a77f4d73e124
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-02-22 14:21:01 +0400

    Remove unnecessary return statements

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -21,8 +21,8 @@ public final class CoreDataFeedStore: FeedStore {
   public func retrieve(completion: @escaping RetrievalCompletion) {
     perform { context in
       completion(Result {
-        try ManagedCache.find(in: context).map { cache in
-          CachedFeed(feed: cache.localFeed, timestamp: cache.timestamp)
+        try ManagedCache.find(in: context).map {
+          CachedFeed(feed: $0.localFeed, timestamp: $0.timestamp)
         }
       })
     }
