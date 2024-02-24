9c89c7c94ab822c8f22396c281234da62c4482cb
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-02-24 13:39:00 +0400

    Remove unused debugging code that was previously used during UI tests

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -13,16 +13,15 @@ import CoreData
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   var window: UIWindow?
   
-  let localStoreURL = NSPersistentContainer
-    .defaultDirectoryURL()
-    .appendingPathComponent("feed-store.sqlite")
-  
   private lazy var httpClient: HTTPClient = {
     URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
   }()
 
   private lazy var store: FeedStore & FeedImageDataStore = {
-    try! CoreDataFeedStore(storeURL: localStoreURL)
+    try! CoreDataFeedStore(
+      storeURL: NSPersistentContainer
+        .defaultDirectoryURL()
+        .appendingPathComponent("feed-store.sqlite"))
   }()
 
   convenience init(httpClient: HTTPClient, store: FeedStore & FeedImageDataStore) {
@@ -46,7 +45,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     let localFeedLoader = LocalFeedLoader(store: store, currentDate: Date.init)
     let localImageLoader = LocalFeedImageDataLoader(store: store)
-
+    
     window?.rootViewController = UINavigationController(rootViewController: FeedUIComposer.feedComposeWith(
       feedLoader: FeedLoaderWithFallbackComposite(
         primary: FeedLoaderCacheDecorator(
@@ -59,9 +58,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
           decoratee: remoteImageLoader,
           cache: localImageLoader))))
   }
-  
-  func makeRemoteClient() -> HTTPClient {
-      return URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
-    }
-  }
+}
 
