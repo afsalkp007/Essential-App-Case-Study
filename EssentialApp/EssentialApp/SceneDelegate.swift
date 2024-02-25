1bfd7635bf15fddb639a6215a13455dd4367b221
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-02-25 14:39:47 +0400

    fixes

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -17,6 +17,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   private lazy var httpClient: HTTPClient = {
     URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
   }()
+  
+  let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
+  
+  private lazy var remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: httpClient)
 
   private lazy var store: FeedStore & FeedImageDataStore = {
     try! CoreDataFeedStore(
@@ -58,10 +62,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   }
   
   private func makeRemoteFeedLoaderWithLocalFallback() -> FeedLoader.Publisher {
-    let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
-
-      let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: httpClient)
-
       return remoteFeedLoader
           .loadPublisher()
           .caching(to: localFeedLoader)
