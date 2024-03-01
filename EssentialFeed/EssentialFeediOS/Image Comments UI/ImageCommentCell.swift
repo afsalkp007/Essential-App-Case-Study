b9bfed4d03e5f5cb34dd8117b03a4d92dd6ace98
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-03-01 16:02:31 +0400

    Implement Image Comments UI

---
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/ImageCommentCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImageCommentCell.swift
+//  EssentialFeediOS
+//
+//  Created by Afsal on 01/03/2024.
+//
+
+import UIKit
+
+public final class ImageCommentCell: UITableViewCell {
+    @IBOutlet private(set) public var messageLabel: UILabel!
+    @IBOutlet private(set) public var usernameLabel: UILabel!
+    @IBOutlet private(set) public var dateLabel: UILabel!
+}
+ 
