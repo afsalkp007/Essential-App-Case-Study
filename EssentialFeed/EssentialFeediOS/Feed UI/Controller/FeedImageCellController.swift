6992ff6598b6c28ab692092ad9fb38070ad2b6ab
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-03-01 12:36:19 +0400

    Add CellController protocol in preparation to support any cell cont…

--- EssentialFeed/EssentialFeediOS/Feed UI/Controller/FeedImageCellController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controller/FeedImageCellController.swift
@@ -13,7 +13,7 @@ public protocol FeedImageCellControllerDelegate {
   func didCancelImageRequest()
 }
 
-public final class FeedImageCellController: ResourceView, ResourceLoadingView, ResourceErrorView {
+public final class FeedImageCellController: CellController, ResourceView, ResourceLoadingView, ResourceErrorView {
   public typealias ResourceViewModel = UIImage
   
   private let viewModel: FeedImageViewModel
@@ -25,7 +25,7 @@ public final class FeedImageCellController: ResourceView, ResourceLoadingView, R
     self.delegate = delegate
   }
   
-  func view(in tableView: UITableView) -> UITableViewCell {
+  public func view(in tableView: UITableView) -> UITableViewCell {
     cell = tableView.dequeueReusableCell()
     cell?.locationContainer.isHidden = !viewModel.hasLocation
     cell?.locationLabel.text = viewModel.location
@@ -50,7 +50,7 @@ public final class FeedImageCellController: ResourceView, ResourceLoadingView, R
     cell?.feedImageRetryButton.isHidden = viewModel.message == nil
   }
     
-  func preload() {
+  public func preload() {
     delegate.didRequestImage()
   }
   
@@ -59,7 +59,7 @@ public final class FeedImageCellController: ResourceView, ResourceLoadingView, R
     cell = nil
   }
   
-  func cancelLoad() {
+  public func cancelLoad() {
     releaseCellForReuse()
     delegate.didCancelImageRequest()
   }
